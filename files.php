8c6776d637c8b892330dd60b52ffa586f14c6981
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-10-21 02:33:09 +0800

    file listing php

---
+++ files.php
@@ -0,0 +1,23 @@
+<?php
+
+include('config.php');
+
+$file_lists = array('attachments');
+
+if (!isset($_POST['name']) || !substr_compare($_POST['name'], '[]', -2) === 0) {
+  http_response_code(400);
+  echo 'Invalid request';
+  exit;
+}
+
+$dir = substr($_POST['name'], 0, -2);
+
+if (!in_array($dir, $file_lists)) {
+    http_response_code(400);
+    echo 'Invalid file list';
+    exit;
+}
+
+echo json_encode(dir_update_time($dir));
+
+?>
