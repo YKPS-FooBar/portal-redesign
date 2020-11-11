e6914edfc7ecba4ea848cecba5c316de7b840607
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-11-11 18:48:11 +0800

    redo navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,50 +1,44 @@
 <template>
-  <div class="center">
-    <vs-navbar target-scroll="#padding-scroll-content" padding-scroll center-collapsed v-model="active">
-      <template #left>
-        <vs-navbar-title class="nav-logo">
-          <img src="../../public/images/icons/ykpao.svg" class="logo">
-        </vs-navbar-title>
-        <vs-navbar-item class="nav-title" style="color: #5cb531 !important; font-size: 14pt;">
-          YK Pao School Portal
-        </vs-navbar-item>
-      </template>
-      <template #right>
-        <router-link class="nav-item" v-for="link in links" :key="link.id" :to="link.link">
-          <vs-navbar-item :active="active == link.id" @click="active = link.id">
-            {{ link.title }}
-          </vs-navbar-item>
-        </router-link>
-<!--        <p style="margin-right: 1em; color: #aaa;">Dark Mode</p>-->
-<!--        <vs-switch v-model="active2">-->
-<!--          <template #circle>-->
-<!--            <i v-if="!active2" class='bx bxs-moon' ></i>-->
-<!--            <i v-else class='bx bxs-sun' ></i>-->
-<!--          </template>-->
-<!--        </vs-switch>-->
-      </template>
-    </vs-navbar>
+  <div class="nav">
+    <!-- <vs-navbar-title class="nav-logo"> -->
+      <!-- <img src="../../public/images/icons/ykpao.svg" class="logo"> -->
+    <!-- </vs-navbar-title> -->
+    <router-link to="/">
+      <img src="../../public/images/icons/ykpao.svg" class="logo">
+    </router-link>
+    <router-link to="/" class="nav-title">
+      YK Pao School Portal
+    </router-link>
+
+    <!-- <router-link class="nav-item" v-for="link in links" :key="link.id" :to="link.link">
+      <vs-navbar-item :active="active == link.id" id="link.id">
+        {{ link.name }}
+      </vs-navbar-item>
+    </router-link> -->
+
+    <router-link class="nav-item" v-for="link in links" :key="link.name" :to="link.link" :class="[link.name === $route.name ? 'active' : '']">
+      {{ link.name }}
+    </router-link>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      active: 0,
       links: [
         {
           id: 0,
-          title: 'Dashboard',
+          name: 'Dashboard',
           link: '/'
         },
         {
           id: 1,
-          title: 'Clubs',
+          name: 'Clubs',
           link: '/clubs'
         },
         {
           id: 2,
-          title: 'Links',
+          name: 'Links',
           link: '/links'
         }
       ]
@@ -53,74 +47,49 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+.nav {
+  margin: 1.5em 2em;
+
+  @media screen and (max-width: 640px) {
+    margin: 1.5em .5em;
+  }
+
+  display: flex;
+  align-items: center;
+  text-align: left;
+}
+
 .logo {
-  margin-left: 2em;
   max-width: 5em;
   height: 2em;
-  margin-top: 2pt !important;
+  margin: 2pt 0 0 1em;
 }
 
 .nav-item {
-  font-size: 12pt !important;
-  font-weight: 400 !important;
-  text-decoration: none !important;
-  color: black;
-}
+  margin: 0 1em;
+  font-size: 12pt;
+  text-decoration: none;
+  color: #000a;
+  font-weight: bold;
+  transition: border-bottom-color .5s ease, color .5s ease;
+  border-bottom-color: transparent;
 
-.vs-button-primary {
-  margin-left: .5em !important;
-}
-
-.vs-button-primary-label {
-  font-size: 12pt !important;
-  text-decoration: none !important;
-  color: #fff;
-}
-
-@media screen and (max-width: 1080px) {
-  .navbar-wrapper {
-    padding: 1.5em 5em;
-    display: flex;
-  }
-
-  .nav-item-wrapper {
-    padding: 1em 0 !important;
-    justify-self: center !important;
-    margin-left: -2.5em !important;
-  }
-
-  .vs-button-primary {
-    margin-top: 1.5em !important;
-    margin-bottom: 1.5em !important;
-    margin-left: .5em !important;
+  &.active {
+    color: black;
+    border-bottom: 2px solid black;
+    margin-bottom: -2px;
   }
 }
 
-@media screen and (max-width: 800px) {
-  .logo {
-    margin-left: 0.5em;
-  }
-  .navbar-wrapper {
-    padding: 1.5em 0.5em;
-    display: flex;
-  }
+.nav-title {
+  @extend .nav-item;
+  flex: auto;
+  font-size: 16pt;
+  color: #5cb531;
 
-  .nav-item-wrapper {
-    padding: 1em 0 !important;
-    margin-top: 3pt !important;
-    margin-left: 0 !important;
-  }
-
-  .vs-button-primary {
-    margin-top: 1.5em !important;
-    margin-bottom: 2em !important;
-    margin-left: .5em !important;
-  }
-}
-
-@media screen and (max-width: 640px) {
-  .nav-title {
+  @media screen and (max-width: 690px) {
     display: none;
   }
 }
