41e634d9c97e5e5e5790278fba3b7e678c927f20
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-11-12 11:24:07 +0800

    simplify dashbar links

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,4 @@
 <template>
-<!--  <div class="d-flex dashboard">-->
-<!--    <div class="heading-wrapper">-->
-<!--      <h1>Dashboard</h1>-->
-<!--    </div>-->
-<!--    <div class="content-wrapper justify-content-center">-->
-<!--      <vs-row vs-justify="center" vs-align="center" class="d-flex card-wrapper">-->
-<!--        <vs-card class="link-card" primary>-->
-<!--          <template #img>-->
-<!--            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">-->
-<!--              <a href="https://powerschool.ykpaoschool.cn/public/" style="color: #000; text-decoration: none !important;">-->
-<!--                <div style="width: 100%;">-->
-<!--                  <img src="../../public/images/icons/powerschool.svg" class="link-icon" style="width: 100% !important;"/>-->
-<!--                </div>-->
-<!--                <h3 style="word-wrap: break-spaces; width: 100%">PowerSchool</h3>-->
-<!--              </a>-->
-<!--            </div>-->
-<!--          </template>-->
-<!--        </vs-card>-->
-<!--      </vs-row>-->
-<!--    </div>-->
-<!--  </div>-->
-
   <div class="d-flex dashboard">
     <div class="heading-wrapper">
       <h1>Dashboard</h1>
@@ -30,147 +8,46 @@
         <h2>Latest Info</h2>
       </div>
       <vs-row vs-justify="center" vs-align="center" class="d-flex card-wrapper">
-        <vs-card @click="active=!active" class="link-card bulletin-card" primary>
+
+        <vs-card v-for="file in files" :key="file.id" @click="file.active = true" class="link-card bulletin-card" primary>
           <template #img>
             <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
               <a style="color: #000; text-decoration: none !important;">
                 <div style="width: 100%;">
                   <p style="line-height: 1.75;"><br></p>
-                  <img src="../../public/images/icons/dailybulletin.svg" class="link-icon link-icon-large" style="width: 100% !important;"/>
+                  <img :src="file.icon" class="link-icon link-icon-large" style="width: 100% !important;"/>
                 </div>
               </a>
             </div>
           </template>
         </vs-card>
-        <vs-dialog overflow-hidden full-screen blur not-padding v-model="active">
+
+        <vs-dialog v-for="file in files" :key="`dialog-${file.id}`" overflow-hidden full-screen blur not-padding v-model="file.active">
           <div class="con-content">
-            <iframe class="embed-responsive-item" src="./images/icons/daily-bulletin.pdf" scrolling="auto" height="1070"></iframe>
+            <iframe class="embed-responsive-item" :src="file.location" scrolling="auto"></iframe>
           </div>
         </vs-dialog>
-        <vs-card @click="active2=!active2" class="link-card updates-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <p style="line-height: 1.75;"><br></p>
-                  <img src="../../public/images/icons/newsupddates.svg" class="link-icon link-icon-large" style="width: 100% !important;"/>
-                </div>
-              </a>
-            </div>
-          </template>
-        </vs-card>
-        <vs-dialog overflow-hidden full-screen blur not-padding v-model="active2">
-          <div class="con-content">
-            <iframe class="embed-responsive-item" src="./images/icons/news-updates.pdf" scrolling="auto" height="1070"></iframe>
-          </div>
-        </vs-dialog>
+
       </vs-row>
       <div class="subheading-wrapper">
         <h2>Common Links</h2>
       </div>
       <vs-row vs-justify="center" vs-align="center" class="d-flex card-wrapper">
-        <vs-card class="link-card" primary>
+
+        <vs-card v-for="link in homeLinks" :key="link.name" class="link-card" primary>
           <template #img>
             <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://powerschool.ykpaoschool.cn/public/" style="color: #000; text-decoration: none !important;">
+              <a :href="link.url" style="color: #000; text-decoration: none !important;">
                 <div style="width: 100%;">
-                  <img src="../../public/images/icons/powerschool.svg" class="link-icon" style="width: 100% !important;"/>
+                  <img v-if="link.icon" :src="require(`../../public/images/icons/${link.icon}`)" class="link-icon" style="width: 100% !important;">
                 </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">PowerSchool</h3>
+                <h3 style="word-wrap: break-spaces; width: 100%">{{ link.name }}</h3>
               </a>
             </div>
           </template>
         </vs-card>
-        <vs-card class="link-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://powerschool.ykpaoschool.cn/teachers/pw.html" style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <img src="../../public/images/icons/powerschool.svg" class="link-icon" style="width: 100% !important;"/>
-                </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">PowerTeacher</h3>
-              </a>
-            </div>
-          </template>
-        </vs-card>
-        <vs-card class="link-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://ykpaoschool.learning.powerschool.com/" style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <img src="../../public/images/icons/Power_School.svg" class="link-icon" style="width: 100% !important;"/>
-                </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">PS Learning</h3>
-              </a>
-            </div>
-          </template>
-        </vs-card>
-        <vs-card class="link-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://ykps.managebac.cn/" style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <img src="../../public/images/icons/managebac.svg" class="link-icon" style="width: 100% !important;"/>
-                </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">ManageBac</h3>
-              </a>
-            </div>
-          </template>
-        </vs-card>
-        <vs-card class="link-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://ykpaoschool.learning.powerschool.com/" style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <img src="../../public/images/icons/outlook.svg" class="link-icon" style="width: 100% !important;"/>
-                </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">Mail</h3>
-              </a>
-            </div>
-          </template>
-        </vs-card>
-        <vs-card class="link-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://outlook.office.com/owa/?realm=ykpaoschool.cn#path=/calendar/view/month" style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <img src="../../public/images/icons/calendar.svg" class="link-icon" style="width: 100% !important;"/>
-                </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">Calendar</h3>
-              </a>
-            </div>
-          </template>
-        </vs-card>
-        <vs-card class="link-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://ykpaoschool-my.sharepoint.com/_layouts/15/MySite.aspx?MySiteRedirect=AllDocuments" style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <img src="../../public/images/icons/onedrive.svg" class="link-icon" style="width: 100% !important;"/>
-                </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">OneDrive</h3>
-              </a>
-            </div>
-          </template>
-        </vs-card>
-        <vs-card class="link-card" primary>
-          <template #img>
-            <div class="d-flex link-icon-wrapper justify-items-center justify-content-center">
-              <a href="https://seasia.mychq.net/ykpao" style="color: #000; text-decoration: none !important;">
-                <div style="width: 100%;">
-                  <img src="../../public/images/icons/chq.svg" class="link-icon" style="width: 100% !important;"/>
-                </div>
-                <h3 style="word-wrap: break-spaces; width: 100%">CHQ</h3>
-              </a>
-            </div>
-          </template>
-        </vs-card>
+
       </vs-row>
-    </div>
-    <div class="footer-wrapper">
-      <span class="footer">Designed by <a class="footer-link" href="https://tigerfeng.cn">Tiger Feng</a>
-        <span style="opacity: 0">--</span>|<span style="opacity: 0">--</span>Developed by <a class="footer-link" href="https://github.com/orgs/YKPS-FooBar/">The Foo Bar</a>
-      </span>
     </div>
   </div>
 </template>
@@ -179,24 +56,31 @@
 import links from "@/data/links.json";
 export default {
   name: 'Dashboard',
-  props: ['name','icon'],
-  data:() => ({
-    active: false,
-    active2: false,
-    input1: '',
-    input2: '',
-    checkbox1: false
-  }),
+  props: ['name', 'icon'],
+  data() {
+    return {
+      // files to show in "Latest Info"
+      files: [
+        {
+          id: 0,
+          icon: require('../../public/images/icons/dailybulletin.svg'),
+          location: '/images/icons/daily-bulletin.pdf',
+          active: false
+        },
+        {
+          id: 1,
+          icon: require('../../public/images/icons/newsupddates.svg'),
+          location: '/files/news-updates.pdf',
+          active: false
+        }
+      ]
+    };
+  },
   components: {
   },
   computed: {
-    links() {
-      return links
-          .map(el => {
-            return {
-              ...el,
-            }
-          })
+    homeLinks() {
+      return links.filter(link => link.home);
     }
   }
 }
@@ -221,23 +105,6 @@ vs-card:hover {
   text-align: left !important;
   margin-top: 1em;
   margin-bottom: 1.5em;
-}
-.footer-wrapper {
-  margin-bottom: 3em;
-}
-.footer {
-  color: #aaa;
-  font-size: 12pt;
-  font-weight: 400;
-  line-height: 20pt;
-}
-.footer-link {
-  color: #aaa;
-  transition: ease-in-out 200ms;
-}
-.footer-link:hover {
-  color: #444;
-  transition: ease-in-out 200ms;
 }
 .content-wrapper {
   width: calc(100% - 30em);
@@ -278,23 +145,19 @@ vs-card:hover {
 #link-label {
   text-decoration: none !important;
 }
-.vs-dialog__content .vs-dialog--scroll {
-  max-height: 100%;
-}
 .con-content {
+  height: 100%;
   max-height: 100vh;
   overflow: hidden;
-  /* 16:9 aspect ratio */
-  padding-top: 56.25%;
   position: relative;
 }
 .con-content iframe {
   border: 0;
+  width: 100%;
   height: 100%;
-  left: 0;
   position: absolute;
   top: 0;
-  width: 100%;
+  left: 0;
 }
 
 @media screen and (max-width: 1280px) {
@@ -342,11 +205,6 @@ vs-card:hover {
     text-align: left !important;
     margin-top: 2em;
     margin-bottom: 1em;
-  }
-  .footer {
-    color: #aaa !important;
-    font-size: 14pt;
-    font-weight: 400;
   }
   .content-wrapper {
     width: calc(100% - 10em);
@@ -411,11 +269,6 @@ vs-card:hover {
   .link-icon-large {
     max-height: 12em !important;
     max-width: 12em !important;
-  }
-  .footer {
-    color: #aaa !important;
-    font-size: 12pt;
-    font-weight: 400;
   }
 }
 @media screen and (min-width: 400px) {
