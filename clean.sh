4b40e6ca41d578cb832e336cbfa18783bde9134e
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-11-17 05:59:24 +0800

    should quote

--- clean.sh
+++ clean.sh
@@ -2,13 +2,13 @@ cd "$(dirname "$0")"
 
 rm -rf node_modules
 rm -rf uploads
-find . -name *.html -delete
-find . -name *.js -delete
-find . -name *.css -delete
-find . -name *.pdf -delete
-find . -name *.map -delete
-find . -name *.png -delete
-find . -name *.svg -delete
-find . -name *.jpg -delete
-find . -name *.ico -delete
 rm -rf dist css js img images
+find . -name '*.html' -delete
+find . -name '*.js' -delete
+find . -name '*.css' -delete
+find . -name '*.pdf' -delete
+find . -name '*.map' -delete
+find . -name '*.png' -delete
+find . -name '*.svg' -delete
+find . -name '*.jpg' -delete
+find . -name '*.ico' -delete
