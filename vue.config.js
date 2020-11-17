82638038482ac44841e6a1cf6cf266e272a5e416
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-11-17 11:57:59 +0800

    yay \?

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   chainWebpack: config => {
-    config.module.rule('pdf').test(/\.pdf$/i).use('file-loader').loader('file-loader')
     config.plugin('html').tap(args => {
       args[0].title = 'YKPS Portal'
       return args
