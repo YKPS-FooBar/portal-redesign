ebcb5a01cfd8922bcc75f21ef1f81e6e36c0956c
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-11-18 13:49:34 +0800

    clear after build

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,20 @@ then
     # clone to backend/
     echo "No backend/ directory detected. Cloning backend branch from origin..."
     git clone --single-branch --branch php $(git config remote.origin.url) backend/
-else
-    # clean backend/ of built files
-    echo "Cleaning backend built files..."
-    find backend/ -type f \
-        \( -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.pdf" -o -name "*.map" \
-           -o -name "*.png" -o -name "*.svg" -o -name "*.jpg" -o -name "*.ico" \) \
-        -delete
-    rm -rf backend/css backend/images backend/img backend/js backend/uploads
 fi
 
 echo "Building frontend..."
 yarn install && yarn build
+
+# clean backend/ of built files
+echo "Cleaning backend built files..."
+find backend/ -type f \
+    \( -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.pdf" -o -name "*.map" \
+       -o -name "*.png" -o -name "*.svg" -o -name "*.jpg" -o -name "*.ico" \) \
+    -delete
+rm -rf backend/css backend/images backend/img backend/js backend/uploads
+
+echo "Moving built files to backend/..."
 cp -r dist/ backend/
 
 echo "The backend is prepared at $(pwd)/backend/"
