cf3d99eaf8ca603d3fa86496ac0d97031b51d5c7
hanwenzhu <thomas.zhu.sh@hotmail.com>
2020-11-22 19:32:21 +0800

    fix display bug on webkit and firefox

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,11 +3,9 @@
   <!-- but it ensures that the hitbox of <a> & tab sequence & stylesheet don't mess up -->
   <div class="mb-5">
     <b-card no-body class="font-weight-bold text-center" style="width: 12em; height: 12em;">
-      <b-link :href="link" v-b-modal="modal" class="flex-grow-1 d-flex text-decoration-none text-reset">
-        <b-card-body class="d-flex flex-column align-items-center hover-shadow">
-          <div class="flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-            <b-card-img :src="require(`../../public/images/icons/${icon}`)" :alt="name" style="object-fit: contain;" :style="named ? 'max-width: 75%; max-height: 75%;' : 'max-width: 100%; max-height: 100%;'"></b-card-img>
-          </div>
+      <b-link :href="link" v-b-modal="modal" class="flex-grow-1 h-100 d-flex text-decoration-none text-reset">
+        <b-card-body class="d-flex flex-column align-items-stretch justify-content-between hover-shadow">
+          <b-card-img :src="require(`../../public/images/icons/${icon}`)" :alt="named ? 'Icon' : name" class="flex-grow-1 flex-shrink-1" :class="named ? 'p-3 h-75' : ''" style="object-fit: contain;"></b-card-img>
           <b-card-text v-if="named">
             {{ name }}
           </b-card-text>
